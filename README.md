eec0e9fbbdfe80a22e84e48b45184d3ed682fbec
nchchow <nelsonchow91.nc@gmail.com>
2020-04-07 18:32:06 -0700

    Updated README.md and minor spacing formatting

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-This project is to display weather data from an api using JavaScript, Html, and CSS
+# Weather App
+
+View the weather based on your current location using data collected from darksky API.
+
+## Motivation
+
+This project is to demonstrate the use of a public API to collect data and to display them using a public library
+
+## Screenshot
+
+<img width="1280" alt="ss_weather_app" src="https://user-images.githubusercontent.com/45683565/78734479-3f2a0980-78fd-11ea-8be5-88b3bf7c9da2.png">
+
+## Tech Stack
+
+Public API: Dark Sky | https://darksky.net/dev
+
+Public library: Skycons | https://darkskyapp.github.io/skycons/
+
+JS, CSS, HTML
+
+## Credits
 
 Tutorial from: https://www.youtube.com/watch?v=wPElVpR1rwA
+
+Dev Ed | https://www.youtube.com/channel/UClb90NQQcskPUGDIXsQEz5Q/about
--- app.js
+++ app.js
@@ -1,69 +1,71 @@
 // console.log("start");
 
 window.addEventListener("load", () => {
-    let long;
-    let lat;
-    const temperatureDescription = document.querySelector('.temperature-description');
-    const temperatureDegree = document.querySelector('.temperature-degree');
-    const locationTimezone = document.querySelector('.location-timezone');
-    const temperatureSection = document.querySelector('.temperature-section');
-    const temperatureSpan = document.querySelector('.temperature-section span');
+  let long;
+  let lat;
+  const temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  const temperatureDegree = document.querySelector(".temperature-degree");
+  const locationTimezone = document.querySelector(".location-timezone");
+  const temperatureSection = document.querySelector(".temperature-section");
+  const temperatureSpan = document.querySelector(".temperature-section span");
 
-    // console.log("loaded");
+  // console.log("loaded");
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = `${proxy}https://api.darksky.net/forecast/0ee7e1f4b8086e05b69e8791e887d49c/${lat},${long}`;
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/0ee7e1f4b8086e05b69e8791e887d49c/${lat},${long}`;
 
-            fetch(api)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    const {temperature, summary, icon} = data.currently;
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const { temperature, summary, icon } = data.currently;
 
-                    // set DOM elements from the api
-                    setDOM(temperature, summary, data);
+          // set DOM elements from the api
+          setDOM(temperature, summary, data);
 
-                    // FORMULA FOR CELSIUS
-                    let celsius = (temperature - 32) * 5 / 9;
+          // FORMULA FOR CELSIUS
+          let celsius = ((temperature - 32) * 5) / 9;
 
-                    // set icon
-                    setIcon(icon, document.querySelector('.icon'));
+          // set icon
+          setIcon(icon, document.querySelector(".icon"));
 
-                    // change temperature to C/F
-                    addTemperatureClickEvent(celsius, temperature);
-                })
+          // change temperature to C/F
+          addTemperatureClickEvent(celsius, temperature);
         });
-    }
-    
-    // set DOM elements from the api
-    function setDOM(temperature, summary, data) {
+    });
+  }
+
+  // set DOM elements from the api
+  function setDOM(temperature, summary, data) {
+    temperatureDegree.textContent = temperature;
+    temperatureDescription.textContent = summary;
+    locationTimezone.textContent = data.timezone;
+  }
+
+  function setIcon(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
+
+  function addTemperatureClickEvent(celsius, temperature) {
+    temperatureSection.addEventListener("click", () => {
+      if (temperatureSpan.textContent === "F") {
+        temperatureSpan.textContent = "C";
+        temperatureDegree.textContent = Math.floor(celsius);
+      } else {
+        temperatureSpan.textContent = "F";
         temperatureDegree.textContent = temperature;
-        temperatureDescription.textContent = summary;
-        locationTimezone.textContent = data.timezone;
-    }
-
-    function setIcon(icon, iconID) {
-        const skycons = new Skycons({color: "white"});
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-        skycons.play();
-        return skycons.set(iconID, Skycons[currentIcon]);
-    }
-
-    function addTemperatureClickEvent(celsius, temperature) {
-        temperatureSection.addEventListener('click', () => {
-            if(temperatureSpan.textContent === 'F') {
-                temperatureSpan.textContent = 'C';
-                temperatureDegree.textContent = Math.floor(celsius);
-            } else {
-                temperatureSpan.textContent = 'F';
-                temperatureDegree.textContent = temperature;
-            }
-        });
-    }
+      }
+    });
+  }
 });
